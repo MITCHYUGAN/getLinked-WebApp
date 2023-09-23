d46d5b4dcac0ac21edba981751a71d3e270b24a8
unknown <denmichigan123@gmail.com>
2023-09-23 23:15:13 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Example alias for components
+      '@components': '/src/components',
+    },
+  },
 })
