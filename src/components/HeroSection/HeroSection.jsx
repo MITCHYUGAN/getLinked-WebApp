6a94c6443eb40134c794556e03667e0f932600d5
unknown <denmichigan123@gmail.com>
2023-09-24 04:37:44 +0100

    first commit

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,5 +1,5 @@
 import heroImg from "./assets/landing-img.png"
-// import gradient from "./assets/gradient.png"
+import chain_spark from "./assets/chain_spark.png"
 import line from "./assets/curvedline.png"
 import timer from "./assets/timer.png"
 import bulb from "./assets/bulb.png"
@@ -7,25 +7,38 @@ import bulb from "./assets/bulb.png"
 const HeroSection = () => {
     return(
         <section className="herosection">
-            {/* <img src={gradient} className="gradient" /> */}
-            <div className="text-line">
-                <h1>Igniting a Revolution in Hr Innovation</h1>
-                <img src={line} alt="Curved Line" />
-            </div>
             <div className="herosectionwrapper">
+                <div className="text-line">
+                    <h1>Igniting a Revolution in HR Innovation</h1>
+                    <img src={line} alt="Curved Line" />
+                </div>
                 <div className="herosectioncontents">
                     <div className="herosectioncontent">
-                        <img src={bulb} className="bulb" alt="" />
-                        {/* <h1>getlinked Tech Hackathon <span>1.0</span></h1> */}
-                        {/* <p>Participate in getlinked tech Hackathon 2023 stand a chance to win a Big prize</p> */}
-                        {/* <button className="button">Register</button> */}
+                        <div className="heresectionheadingtext_chain_spark">
+                            <h1>getlinked Tech Hackathon <span>1.0</span></h1>
+                            <img src={chain_spark} alt="" />
+                        </div>
+                        <div className="heresectionheadingtext_chain_sparkmobile">
+                            <h1>getlinked Tech</h1>
+                            <div>
+                                <h1>Hackathon <span>1.0</span></h1>
+                                <img src={chain_spark} alt="" />
+                            </div>
+                        </div>
+                        <p>
+                            Participate in getlinked tech Hackathon 2023 stand 
+                            a chance to win a Big prize
+                        </p>
+                        <a href="/register">
+                            <button className="button">Register</button>
+                        </a>
                     </div>
                     <div className="timer">
-                        <img src={timer} alt="Timer Image" />
+                        <img src={timer} alt="" />
                     </div>
                 </div>
                 <div className="herosectionimg">
-                    <img src={heroImg} alt="Hero Section Image" />
+                    <img src={heroImg} alt="" />
                 </div>
             </div>
         </section>
--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -4,13 +4,13 @@ const Partners = () => {
     return(
         <section className="partners">
             <div className="partnerswrapper">
-                {/* <div className="partnersheader">
+                <div className="partnersheader">
                     <h1>Partners and Sponsors</h1>
                     <p>Getlinked Hackathon 1.0 is honored to have the following major companies as its partners and sponsors</p>
-                </div> */}
-                {/* <div className="partnerscontents">
+                </div>
+                <div className="partnerscontents">
                     <img className="partnersimg"  src={img} alt="" />
-                </div> */}
+                </div>
             </div>
         </section>
     )
